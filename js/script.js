3fab07560f04cde5ceb31f30959376acddca624f
Anna Wroblewska <a.j.wroblewska@gmail.com>
2019-11-24 20:28:06 +0100

    add rwd

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var showMenu = document.querySelector(".show-menu");
+var mobileView = document.querySelector(".mobile-view");
+
+showMenu.addEventListener('click', function () {
+    showMenu.classList.toggle(".mobile-view");
+});
